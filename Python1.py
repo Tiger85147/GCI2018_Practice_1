4142e24e7c2f494dabd244aef674d23c1ea185fc
Tiger85147 <44669228+Tiger85147@users.noreply.github.com>
2018-11-01 16:10:15 +0530

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,12 @@
+a=1
+while a<=10:
+      print("GCI is great")
+      a+=1
+      
+      
+print("Enter your name")
+x= input()
+
+y= x [::-1]
+
+print(" Hello "+ x +", pleased to meet you! Did you know your name backwards is "+ y)
